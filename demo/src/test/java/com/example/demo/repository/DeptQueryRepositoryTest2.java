78af8884b608cc6d87b7e6fcf3510285c6706eb2
OshitaTatsuya <oshita.tatsuya@tis.co.jp>
2022-04-07 07:02:41 +0900

    mod request annotation

--- demo/src/test/java/com/example/demo/repository/DeptQueryRepositoryTest2.java
+++ demo/src/test/java/com/example/demo/repository/DeptQueryRepositoryTest2.java
@@ -6,10 +6,12 @@ import com.example.demo.application.domain.repositories.DeptQueryRepository;
 import com.example.demo.repository.base.QueryBaseRepositoryTest;
 
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 public class DeptQueryRepositoryTest2 extends QueryBaseRepositoryTest<DeptQueryRepository, DeptQueryRepositoryTest2> {
 
     @Test
+    @DisplayName("▼▼▼▼▼ test001 DisplayName ▼▼▼▼▼")
     public void test0001() {
 
         List<DeptQuery> resultList = this.repositoryTest.findTest(1);
