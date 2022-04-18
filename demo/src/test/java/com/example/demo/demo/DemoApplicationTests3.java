a561abda82002f833d07de4436ea70eafe386f71
OshitaTatsuya <oshita.tatsuya@tis.co.jp>
2022-04-19 07:29:18 +0900

    mod init list

--- demo/src/test/java/com/example/demo/demo/DemoApplicationTests3.java
+++ demo/src/test/java/com/example/demo/demo/DemoApplicationTests3.java
@@ -3,7 +3,6 @@ package com.example.demo.demo;
 import org.junit.Test;
 import com.example.demo.application.controller.request.GreetingResponse;
 import com.example.demo.base.RestApiBaseTest;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import com.example.demo.application.common.constants.AppServerServiceURI;
@@ -14,7 +13,6 @@ public class DemoApplicationTests3 extends RestApiBaseTest<DemoApplicationTests3
 	public void setProperty() {
 		super.logCls = DemoApplicationTests3.class;
 		super.responseCls = GreetingResponse.class;
-		super.getList = new ArrayList<GreetingResponse>();
 	}
 
 	@Test
