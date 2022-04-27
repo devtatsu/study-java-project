48861e95c11726022529ca97b487144c01a3bf92
OshitaTatsuya <oshita.tatsuya@tis.co.jp>
2022-04-27 21:06:36 +0900

    add junit test data insert code

--- demo/rest-api/src/test/java/com/example/demo/demo/DemoApplicationTests3.java
+++ demo/rest-api/src/test/java/com/example/demo/demo/DemoApplicationTests3.java
@@ -4,10 +4,19 @@ import org.junit.Test;
 import com.example.demo.application.controller.request.GreetingResponse;
 import com.example.demo.base.RestApiBaseTest;
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
+
 import com.example.demo.application.common.constants.AppServerServiceURI;
 
 public class DemoApplicationTests3 extends RestApiBaseTest<DemoApplicationTests3, String, GreetingResponse> {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public void setProperty() {
@@ -17,7 +26,10 @@ public class DemoApplicationTests3 extends RestApiBaseTest<DemoApplicationTests3
 
 	@Test
 	@SuppressWarnings("unchecked")
+	@Sql(scripts = "/deptquery/setUpData.sql")
 	public void test0001() {
+
+		List<Map<String, Object>> getData = jdbcTemplate.queryForList("SELECT * FROM DEPT");
 
 		ResponseEntity<List<GreetingResponse>> wkResult = (ResponseEntity<List<GreetingResponse>>) super.curlGetList(
 				AppServerServiceURI.GET_GREETING);
--- demo/rest-api/src/test/java/com/example/demo/demo/DemoApplicationTests4.java
+++ demo/rest-api/src/test/java/com/example/demo/demo/DemoApplicationTests4.java
@@ -3,10 +3,20 @@ package com.example.demo.demo;
 import org.junit.Test;
 import com.example.demo.application.resource.Greeting;
 import com.example.demo.base.RestApiBaseTest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
+
 import com.example.demo.application.common.constants.AppServerServiceURI;
 
 public class DemoApplicationTests4 extends RestApiBaseTest<DemoApplicationTests4, String, Greeting> {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public void setProperty() {
@@ -34,6 +44,8 @@ public class DemoApplicationTests4 extends RestApiBaseTest<DemoApplicationTests4
 	public void test0002() {
 
 		super.responseCls = Greeting.class;
+
+		List<Map<String, Object>> getData = jdbcTemplate.queryForList("SELECT * FROM DEPT");
 
 		for (int i = 0; i < 3; i++) {
 			ResponseEntity<Greeting> wkResult = (ResponseEntity<Greeting>) super.curlGet(
